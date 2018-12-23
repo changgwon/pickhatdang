e12bbd66a03fa448bf39c32154b7c1fdd32e9294
changgwon <rnjs9409@naver.com>
2018-12-24 00:48:11 +0900

    object form

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,26 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :tables
+
+  def self.read_restaurant(user_id)
+   return Restaurant.where("user_id = ?",user_id)
+  end
+
+  def self.create_restaurant(params,user_id)
+    @restaurant = Restaurant.new(params)
+    @restaurant.user_id = user_id
+    @restaurant.save
+    return @restaurant
+  end
+
+
+  def self.update_restaurant(params,restaurant_id)
+    @restaurant = Restaurant.find(restaurant_id)
+    @restaurant.update(params)
+  end
+
+  def self.delete_restaurant(restaurant_id)
+    @restaurant=Restaurant.find(restaurant_id)
+    @restaurant.delete
+  end
 end
