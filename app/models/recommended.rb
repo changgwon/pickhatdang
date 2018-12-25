3489c5636063c60672d2848c72a26ca7fd69e2c5
cmptr077 <cmptr077@naver.com>
2018-12-25 11:16:44 +0900

    xmas

--- app/models/recommended.rb
+++ app/models/recommended.rb
@@ -1,3 +1,35 @@
 class Recommended < ApplicationRecord
   belongs_to :user
 end
+
+def self.read_recommended(user_id,r_id)
+  return Recommended.where("user_id = ? AND r_id = ?" , user_id, r_id).last
+end
+
+def self.create_recommended(user_id,r_id)
+  @recommended=Recommended.new
+  @recommended.user_id=user_id
+  @recommended.r_id=r_id
+  @recommended.save
+  return @recommended
+end
+
+def self.set_rwp(recommended,rating,waiting,price)
+  recommended.rating=Math.exp(-0.1*((rating-5)**2))*5
+  recommended.waiting=Math.exp(-0.001*(waiting**2.5))*5
+  recommended.price=Math.exp(-0.0007*((price-4000)/200)**2)*5
+  recommended.save
+end
+
+def self.calculate_score(priority,recommended)
+  dividend=recommended.rating*priority.rating+recommended.price*priority.price+recommended.waiting*priority.waiting
+  sum = priority.rating+priority.waiting+priority.price
+  recommended.score=dividend/sum
+  recommended.save
+end
+
+def self.sort_where(user_id)
+  @user_recommended=Recommended.where(user_id:user_id)
+  @sorted_r=@user_recommended.sort_by { |e| -e[:score] }
+  return @sorted_r
+end
